9f993a7d19a1b44625ff29c910fd45e06e1c4b31
Vojtech Vladyka <vojtech.vladyka@velux.com>
2023-05-15 15:02:43 +0200

    This is initial working version

--- main.ts
+++ main.ts
@@ -1,137 +1,146 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { syntaxTree } from "@codemirror/language";
+import { RangeSetBuilder, EditorSelection } from "@codemirror/state";
+import {
+	Decoration,
+	DecorationSet,
+	EditorView,
+	PluginSpec,
+	PluginValue,
+	ViewPlugin,
+	ViewUpdate,
+	WidgetType,
+} from "@codemirror/view";
+import { Plugin, editorLivePreviewField } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+function selectionAndRangeOverlap(
+	selection: EditorSelection,
+	rangeFrom: number,
+	rangeTo: number
+) {
+	for (const range of selection.ranges) {
+		if (range.from <= rangeTo && range.to >= rangeFrom) {
+			console.log("in range");
+			return true;
+		}
+	}
 
-interface MyPluginSettings {
-	mySetting: string;
+	console.log("outside range");
+	return false;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+export class DotsWidget extends WidgetType {
+	level;
+	maximum;
+
+	constructor(level: number, maximum: number) {
+		super();
+
+		this.level = level;
+		this.maximum = maximum;
+	}
+
+	toDOM(view: EditorView): HTMLElement {
+		const div = document.createElement("span");
+
+
+		for (let i = 0; i < this.maximum; i++) {
+			if (this.level > 0) {
+				div.innerText += "🔴";
+				this.level--;
+			}
+			else {
+				div.innerText += "⚫";
+			}
+
+		}
+
+		return div;
+	}
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class DotsPlugin implements PluginValue {
+	decorations: DecorationSet;
 
-	async onload() {
-		await this.loadSettings();
+	constructor(view: EditorView) {
+		this.decorations = this.buildDecorations(view) ?? Decoration.none;
+	}
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+	update(update: ViewUpdate) {
+		// only activate in LP and not source mode
+		//@ts-ignore
+		if (!update.state.field(editorLivePreviewField)) {
+			this.decorations = Decoration.none;
+			return;
+		}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		if (update.docChanged ||
+			update.viewportChanged ||
+			update.selectionSet) {
+			this.decorations = this.buildDecorations(update.view) ?? Decoration.none;
+		}
+	}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+	destroy() { }
+
+	buildDecorations(view: EditorView): DecorationSet {
+		const builder = new RangeSetBuilder<Decoration>();
+
+		const selection = view.state.selection;
+		const regex = new RegExp(".*?_?inline-code_?.*");
+
+		for (let { from, to } of view.visibleRanges) {
+			syntaxTree(view.state).iterate({
+				from,
+				to,
+				enter(node) {
+					const type = node.type;
+					// markdown formatting symbols
+					if (type.name.includes("formatting")) return;
+					if (!regex.test(type.name)) return;
+
+					// contains the position of node
+					const start = node.from;
+					const end = node.to;
+					// don't continue if current cursor position and inline code node (including formatting
+					// symbols) overlap
+					if (selectionAndRangeOverlap(selection, start, end + 1)) return;
+
+					const original = view.state.doc.sliceString(start, end).trim();
+
+					if (/^dots:\s*([\s\S]+)\s*?/.test(original)) {
+						let [, contents] = /^dots:\s*([\s\S]+)\s*?/.exec(original)!;
+						let [levelDots, howManyDots] = contents.trim().split("/")
+
+						if (!Number.isFinite(+howManyDots)) return;
+
+						builder.add(
+							start,
+							end,
+							Decoration.replace({
+								widget: new DotsWidget(+levelDots, +howManyDots),
+							})
+						);
+
 					}
+				},
+			});
+		}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		return builder.finish();
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+const pluginSpec: PluginSpec<DotsPlugin> = {
+	decorations: (value: DotsPlugin) => value.decorations,
+};
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+export const dotsPlugin = ViewPlugin.fromClass(
+	DotsPlugin,
+	pluginSpec
+);
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+export default class BetterInlineFieldsPlugin extends Plugin {
+	async onload() {
+		this.registerEditorExtension(dotsPlugin);
 	}
 }
